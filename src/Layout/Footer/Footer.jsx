73582242386acf01a8f08ea7583e8c3cddacf39e
ImJaeOne <dlawi0108@naver.com>
2024-09-14 18:01:20 +0900

    feat(#14): footer

---
+++ src/Layout/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <div className="bottom-0 left-0 w-full flex items-center text-white justify-around backdrop-blur-sm bg-black">
+            <div className="py-5">
+                <div>Copyright 2024 EEPL. All Rights Reserved</div>
+                <div>Proudly created with Prof.Joe's lab</div>
+            </div>
+            <div className="py-5">
+                <div>Energy & Environment Polymer Lab</div>
+                <div>Email : chp@gnu.ac.kr</div>
+            </div>
+            <div className="py-5">
+                <div>연구실 : 경남 진주시 내동로 139번길 8번지 내동캠퍼스 601호</div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
