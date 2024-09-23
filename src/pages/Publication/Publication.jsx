26520e2fa2dd62c0780ccdaa3f7b4fdf5e2d7a06
Jaewon Lim <dlawi0108@naver.com>
2024-09-23 21:59:10 +0900

    feat(#18): publication 수정

--- src/pages/Publication/Publication.jsx
+++ src/pages/Publication/Publication.jsx
@@ -5,6 +5,7 @@ import GoogleSheetData from '../../service/Publication/PublicationData';
 
 const Publication = () => {
     const [publicationData, setPublicationData] = useState([]);
+    const [selectedYear, setSelectedYear] = useState(null);
     const yearRef = useRef({});
 
     useEffect(() => {
@@ -13,7 +14,7 @@ const Publication = () => {
             setPublicationData(data);
         };
         getPublicationData();
-    },[])
+    }, []);
 
     const filterPublicationsByYear = (year) => {
         return publicationData.filter((item) => item.year === year);
@@ -21,10 +22,12 @@ const Publication = () => {
 
     const getYears = () => {
         const years = publicationData.map((item) => item.year);
-        return [...new Set(years)].sort((a, b) => b - a);
+        const getYears = [...new Set(years)].sort((a, b) => b - a);
+        return getYears; 
     };
 
     const publicationYear = getYears();
+    const sidebarYears = publicationYear.filter((year) => year >= 2018); 
 
     const yearScroll = (year) => {
         const clickYear = yearRef.current[year];
@@ -38,26 +41,30 @@ const Publication = () => {
                 behavior: 'smooth',
             });
         }
+        setSelectedYear(year);
     };
+
     return (
         <div className="pt-28 pb-10 flex flex-col items-center w-4/5 max-w-screen-lg mx-auto">
             {/* 사이드바 */}
-            <div className="fixed top-1/4 bg-slate-600" style={{ right: '10%' }}>
-                {publicationYear.map((year, index) => (
-                    <PublicationSide key={index} year={year} yearScroll={() => yearScroll(year)} />
-                ))}
+            <div className="fixed top-1/4 bg-slate-600" style={{ right: '0' }}>
+                {sidebarYears.map((year,index) => (
+                        <PublicationSide key={index} year={year} yearScroll={() => yearScroll(year)} isSelected={year === selectedYear} />
+                    )
+                )}
             </div>
             {/* 메인 */}
             <div className="text-4xl font-bold py-8 border-b-2 border-black w-full m-8 text-center">국내외 논문</div>
             <div className="flex flex-col items-center w-full px-4">
-                {publicationYear.map((year, index) => (
-                    <div key={index} ref={(el) => (yearRef.current[year] = el)} className="mb-8 w-full">
-                        <span className="text-3xl font-semibold">{year}</span>
-                        <div className="mt-4 ml-4">
-                            <PublicationCard publicationItems={filterPublicationsByYear(year)} />
+                {publicationYear.map((year,index ) => (
+                        <div key={index} ref={(el) => (yearRef.current[year] = el)} className="mb-8 w-full">
+                            <span className="text-3xl font-semibold">{year}</span>
+                            <div className="mt-4 ml-4">
+                                <PublicationCard publicationItems={filterPublicationsByYear(year)} />
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    )
+                )}
             </div>
         </div>
     );
