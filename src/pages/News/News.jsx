8a7fec0d86fbe1008c224458afcf0eb620c1e476
Cat-Hanbit <hello.git.GitHub@gmail.com>
2024-10-03 16:45:14 +0900

    feat(#25): news 스켈레톤 구현

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,13 +1,103 @@
 import NewsCard from "../../components/Card/NewsCard";
 
+const NewsCardItem = [
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용 tailwind css 어렵네',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+    {
+        imgSrc: '../../assets/images/starship.png',
+        content: '소식과 관련된 내용',
+        day: '2024-07-07',
+        iconSrc: '../../assets/images/viewicon.png',
+        view: '87',
+    },
+]
+
 const News = () => {
     return (
-        <div>
-            <div>
-                newscom
+        <div className="pt-28 pb-10 flex flex-col items-center w-4/5 max-w-screen-lg mx-auto">
+            <div className="text-4xl font-bold py-8 border-b-2 border-black w-full m-8 text-center">소식</div>
+            <div className="flex flex-row flex-wrap justify-center">
+                {NewsCardItem.map((item) => (
+                    <NewsCard item={item} />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default News;
