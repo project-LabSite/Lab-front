02430a0171f562943f6448d46a8ab71e2b376521
Cat-Hanbit <hello.git.GitHub@gmail.com>
2024-10-07 12:36:00 +0900

    feat(#31): contact 스켈레톤 구현

---
+++ src/components/Common/KakaoLocation.jsx
@@ -0,0 +1,23 @@
+//import { useEffect } from "react";
+
+//const {Kakao} = window;
+
+/* 컴포넌트로 작업 */
+/*const KakaoLocation = () => {
+    useEffect(() => {
+        const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const option = {
+        center: new Kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표
+        level: 3
+    };
+    const map = new Kakao.maps.Map(container, option); //지도 생성 및 객체 리턴
+    }, [])
+
+    return (
+        <div>
+            <div id="map" className="w-64 h-52"></div>
+        </div>
+    )
+}
+
+export default KakaoLocation;*/
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from '../Layout/Nav/Nav';
 import Main from '../pages/Main/Main';
 import Publication from '../pages/Publication/Publication';
+import Cotact from '../pages/Contact/Contact';
 import Login from '../pages/Login/Login';
 import Signup from '../pages/Signup/Signup'
 import Footer from '../Layout/Footer/Footer';
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/publication" element={<Publication />} />
                 <Route path="/signin" element={<Login />} />
                 <Route path='/signup' element={<Signup/>}/>
+                <Route path='/contact' element={<Cotact />}/>
             </Routes>
             <Footer />
         </BrowserRouter>
