49a4fce07edaa7880789611b89190f61d4e5379e
taewoo01 <xodn9402@naver.com>
2024-10-21 20:36:15 +0900

    feat(#25): News 스켈레톤 구현

--- src/components/Card/NewsCard.jsx
+++ src/components/Card/NewsCard.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faeye } from '@fortawesome/free-solid-svg-icons';
+
 const NewsCard = ({ item }) => {
     return (
         <div className="bg-slate-600 w-60 h-96 flex flex-col ml-4 mb-8 drop-shadow-lg">
@@ -8,7 +11,7 @@ const NewsCard = ({ item }) => {
                 <div className="font-bold text-xl overflow-hidden text-ellipsis whitespace-nowrap">{item.content}</div>
                 <div className="text-sm flex flex-row justify-end">
                     <div>{item.day}</div>
-                    <img src={item.iconSrc} alt="아이콘 사진" />
+                    
                     <div>{item.view}</div>
                 </div>
             </div>
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -8,89 +8,14 @@ const NewsCardItem = [
         iconSrc: '../../assets/images/viewicon.png',
         view: '87',
     },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
-    {
-        imgSrc: '../../assets/images/starship.png',
-        content: '소식과 관련된 내용',
-        day: '2024-07-07',
-        iconSrc: '../../assets/images/viewicon.png',
-        view: '87',
-    },
 ]
 
 const News = () => {
     return (
-        <div className="pt-28 pb-10 flex flex-col items-center w-4/5 max-w-screen-lg mx-auto">
-            <div className="text-4xl font-bold py-8 border-b-2 border-black w-full m-8 text-center">소식</div>
+        <div className="pt-28 pb-10 flex-col items-center w-4/5 max-w-screen-lg mx-auto">
+            <div className="pl-6 text-4xl text-gray-800 font-medium py-4 border-b-2 border-blue-800 w-full mb-4">
+                소식
+            </div>
             <div className="flex flex-row flex-wrap justify-center">
                 {NewsCardItem.map((item) => (
                     <NewsCard item={item} />
