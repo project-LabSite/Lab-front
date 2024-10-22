e6346936dab4bf1f736f61d3e26898b2f9ea0fff
taewoo01 <xodn9402@naver.com>
2024-10-22 12:33:41 +0900

    feat(#52): NewsInfo 스켈레톤 구현

---
+++ src/pages/NewsInfo/NewsInfo.jsx
@@ -0,0 +1,9 @@
+const NewsInfo = () => {
+    return (
+        <div>
+            <div className="pl-6 text-4xl text-gray-800 font-medium py-4 border-b-2 border-blue-800 w-full mb-4">소식</div>
+        </div>
+    );
+};
+
+export default NewsInfo;
