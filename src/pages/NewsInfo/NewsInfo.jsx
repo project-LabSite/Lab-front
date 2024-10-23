5b8ad081eee30300f013cb113557b6495d934b8f
taewoo01 <xodn9402@naver.com>
2024-10-23 20:27:47 +0900

    feat(#52): NewsInfo 스켈레톤 구현

--- src/pages/NewsInfo/NewsInfo.jsx
+++ src/pages/NewsInfo/NewsInfo.jsx
@@ -1,6 +1,6 @@
 const NewsInfo = () => {
     return (
-        <div>
+        <div className="pt-28 pb-10 flex flex-col items-center w-4/5 max-w-screen-lg mx-auto mb-20">
             <div className="pl-6 text-4xl text-gray-800 font-medium py-4 border-b-2 border-blue-800 w-full mb-4">소식</div>
         </div>
     );
