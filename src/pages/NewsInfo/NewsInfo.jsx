a1f589d00049c61ad49ed2577b31afdee8aca763
taewoo01 <xodn9402@naver.com>
2024-10-23 20:38:19 +0900

    feat(#52): NewsInfo 스켈레톤 구현

--- src/pages/NewsInfo/NewsInfo.jsx
+++ src/pages/NewsInfo/NewsInfo.jsx
@@ -1,7 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+
+
 const NewsInfo = () => {
     return (
         <div className="pt-28 pb-10 flex flex-col items-center w-4/5 max-w-screen-lg mx-auto mb-20">
             <div className="pl-6 text-4xl text-gray-800 font-medium py-4 border-b-2 border-blue-800 w-full mb-4">소식</div>
+            <div>소식과 관련된 내용 작성</div>
+            <div className="text-sm flex flex-row justify-end">
+                    <div>2024-07-07</div>
+                    <FontAwesomeIcon className='m-1' icon={faEye} />
+                    <div>87</div>
+                </div>
         </div>
     );
 };
